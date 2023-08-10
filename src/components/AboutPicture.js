aeb8adfee5d4676ca88fa94eeace3383c496eece
Laurent Callet <arkodark@gmail.com>
2023-08-10 20:32:39 +0200

    Creation and integration of the About page

---
+++ src/components/AboutPicture.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import pictureAbout from '../assets/images/Image_source2.webp';
+
+const AboutPicture = () => {
+  return (
+     <div className='picture-about-wrapper'>
+        <img src={pictureAbout} alt="Vue d'une vallée serpentant entre des monts enneigés" />
+     </div>
+  );
+};
+
+export default AboutPicture;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
+import AboutPicture from '../components/AboutPicture';
+import Footer from '../components/Footer';
 
 const About = () => {
    return (
       <div>
          <Header />
+         <AboutPicture />
+         <Footer />
       </div>
    );
 };
