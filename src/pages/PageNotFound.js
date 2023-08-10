9bbe032ccdf15464086c58752bbadf3ed337c41e
Laurent Callet <arkodark@gmail.com>
2023-08-10 23:29:49 +0200

    Creation of the 404 page

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { NavLink } from 'react-router-dom';
 
 const PageNotFound = () => {
    return (
-      <div>
-         
+      <div className='pnf-container'>
+         <Header />
+         <div className="pnf-body-container">
+            <h2>404</h2>
+            <h3>Oups! La page que vous demandez n'existe pas.</h3>
+            <NavLink to="/">
+               <p>Retourner sur la page d'accueil</p>
+            </NavLink>
+         </div>
+         <Footer />
       </div>
    );
 };
