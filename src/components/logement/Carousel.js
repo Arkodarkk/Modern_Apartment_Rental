3c7a86ce565791f6dfd2b2c8ad5a278743458c6a
Laurent Callet <arkodark@gmail.com>
2023-08-14 00:58:35 +0200

    feat: Creation of the Carousel

--- src/components/logement/Carousel.js
+++ src/components/logement/Carousel.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import leftArrow from '../../assets/images/left_arrow.svg'
+import rightArrow from '../../assets/images/right_arrow.svg'
 
 const Carousel = ({ appartement }) => {
+   const [index, setIndex] = useState(0);
+   const pictures = appartement.pictures
+
+   const handlePrev = () => {
+      if (index === 0) {
+         setIndex(pictures.length - 1);
+      } else {
+         setIndex(index - 1);
+      }
+   };
+   
+   const handleNext = () => {
+      if (index < pictures.length - 1) {
+         setIndex(index + 1);
+      } else {
+         setIndex(0);
+      }
+   };
+
    return (
-      <div>
-         
+      <div className='carousel-container'>
+         <img className='visible-image' src={pictures[index]} alt={`${appartement.title} - ${index + 1}`} />
+         <img className='carousel-arrows left-arrow' src={leftArrow} alt={`Bouton du carousel pour afficher l'image précédente`} onClick={handlePrev} />
+         <img className='carousel-arrows right-arrow' src={rightArrow} alt={`Bouton du carousel pour afficher l'image suivante`} onClick={handleNext} />
+         <p className="carousel-position">{`${index + 1}/${pictures.length}`}</p>
+         {/* {pictures &&
+            pictures.map((picture, index) => 
+               <img key={`Carousel #${index}`} src={picture} alt={`Photo ${index + 1} - ${appartement.title}`} />
+         )} */}
       </div>
    );
 };
--- src/pages/LogementPage.js
+++ src/pages/LogementPage.js
@@ -3,13 +3,12 @@ import { useParams } from "react-router-dom";
 import Header from "../components/header/Header";
 import Carousel from "../components/logement/Carousel";
 import LogementInfos from "../components/logement/LogementInfos";
-import datas from "../assets/database/logements.json";
 import Footer from "../components/footer/Footer";
+import datas from "../assets/database/logements.json";
 
 const LogementPage = () => {
 	const { id } = useParams();
 	const appartement = datas.find((logement) => logement.id === id);
-	console.log(appartement);
 
 	return (
 		<div className="logement-page-container">
