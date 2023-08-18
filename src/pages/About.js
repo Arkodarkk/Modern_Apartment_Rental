2fdfd20dad3b4df47ce99b0c9706205fbb2db60d
Laurent Callet <arkodark@gmail.com>
2023-08-18 14:55:34 +0200

    chore: Move About page data to external file

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,26 +3,19 @@ import Header from "../components/header/Header";
 import AboutPicture from "../components/about/AboutPicture";
 import Footer from "../components/footer/Footer";
 import Collapse from "../components/logement/Collapse";
+import aboutData from '../assets/database/aboutData.json';
 
 const About = () => {
-
-	const fiabilite = "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
-	
-	const respect = "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
-	
-	const service = "Chez Kasa, le service est essentiel. Notre équipe dévouée est là pour répondre à vos besoins, du début à la fin de votre séjour. Chaque aspect de votre expérience est soigneusement orchestré pour vous offrir un moment inoubliable. Vous n'obtenez pas seulement un logement, vous obtenez une expérience de service complète, à la hauteur de vos attentes."
-	
-	const securite = "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
 
 	return (
 		<div>
 			<Header />
 			<AboutPicture />
 			<div className="collapses-wrapper">
-				<Collapse label="Fiabilité" content={fiabilite} customClass="about-collapses" />
-				<Collapse label="Respect" content={respect} customClass="about-collapses" />
-				<Collapse label="Service" content={service} customClass="about-collapses" />
-				<Collapse label="Sécurité" content={securite} customClass="about-collapses" />
+				<Collapse label="Fiabilité" content={aboutData.fiabilite} customClass="about-collapses" />
+				<Collapse label="Respect" content={aboutData.respect} customClass="about-collapses" />
+				<Collapse label="Service" content={aboutData.service} customClass="about-collapses" />
+				<Collapse label="Sécurité" content={aboutData.securite} customClass="about-collapses" />
 			</div>
 			<Footer />
 		</div>
